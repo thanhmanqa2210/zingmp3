1c44e9946aa2cbc5f0a6758a61d262869f2d6ac3
ThanhManLab <Thanhmantm1999@gmail.com>
2021-12-18 21:37:52 +0700

    six

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.scss";
+import "./index.css";
 import App from "./App";
 import PlayMusic from "./components/playMusic/playMusic.footer";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App />
-    <PlayMusic /> */}
-    <h2>XIn chào các</h2>
+    <App />
+    <PlayMusic />
   </React.StrictMode>,
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
