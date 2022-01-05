1aac65afd10ec93915dcb984f4baa3d4c160fe8b
ThanhManLab <Thanhmantm1999@gmail.com>
2022-01-05 20:36:34 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import "./App.css";
 import Container from "./components/container/container";
 import Menu from "./components/menu/menu";
 import BelowMenu from "./components/menu/belowMenu";
-// import { useState } from "react";
+
 import React from "react";
-// import { images } from "./components/data/theme";
+
 import Context from "./components/modals/Context";
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
   const  background  = React.useContext(Context);
   return (
+    // <BrowserRouter>
     <div className="App">
       <div style={{background:`url(${background.background})`}} className="backgr-app"></div>
       <BelowMenu></BelowMenu>
       <Menu></Menu>
       <Container></Container>
     </div>
+    // </BrowserRouter>
   );
 }
 
